676bae2b30bc4083a612e5c56a132c21c66d1fe9
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-07-22 09:41:53 -0700

    test 2 good fail, expect testTitle and receive testTitl

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -20,5 +20,11 @@ namespace VendorTracker.Tests
       Order newOrder = new Order("testTitle", "testDescription", 10, "testDate");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_Order()
+    {
+      Order newOrder = new Order("testTitle", "testDescription", 10, "testDate");
+      Assert.AreEqual("testTitl", newOrder.Title);
+    }
   }
 }
